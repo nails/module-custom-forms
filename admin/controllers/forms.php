11e352cd9bca50d93674a301684744949ecfd846
Pablo de la Peña <p@shedcollective.org>
2015-11-25 14:23:15 +0000

    Naming convention updates

--- admin/controllers/forms.php
+++ admin/controllers/forms.php
@@ -31,11 +31,11 @@ class Forms extends BaseAdmin
     {
         if (userHasPermission('admin:forms:forms:browse')) {
 
-            $navGroup = Factory::factory('Nav', 'nailsapp/module-admin');
-            $navGroup->setLabel('Custom Forms');
-            $navGroup->setIcon('fa-list-alt');
-            $navGroup->addAction('Browse Forms');
-            return $navGroup;
+            $oNavGroup = Factory::factory('Nav', 'nailsapp/module-admin');
+            $oNavGroup->setLabel('Custom Forms');
+            $oNavGroup->setIcon('fa-list-alt');
+            $oNavGroup->addAction('Browse Forms');
+            return $oNavGroup;
         }
     }
 
