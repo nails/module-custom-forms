98cc62050d36ff681acd90f409091c1dd00a1868
Pablo de la Peña <p@shedcollective.org>
2019-11-27 16:53:18 +0000

    Loading FormEdit.js as an admin plugin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,5 +4,9 @@ import '../sass/admin.scss';
 import FormEdit from './components/FormEdit.js';
 
 (function() {
-    new FormEdit();
+    window.NAILS.ADMIN.registerPlugin(
+        'nails/module-custom-forms',
+        'FormEdit',
+        new FormEdit()
+    );
 })();
