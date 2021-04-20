d6acd33d057ec422414965e0d0e5df368f1f5cff
Pablo de la Peña <p@shedcollective.org>
2021-04-20 21:11:50 +0100

    Using Admin floatingControl helper

--- admin/views/Forms/edit.php
+++ admin/views/Forms/edit.php
@@ -231,10 +231,10 @@
             </div>
         </div>
     </section>
-    <div class="admin-floating-controls">
-        <button type="submit" class="btn btn-primary">
-            Save Changes
-        </button>
-    </div>
-    <?=form_close()?>
+    <?php
+
+    echo \Nails\Admin\Helper::floatingControls();
+    echo form_close();
+
+    ?>
 </div>
