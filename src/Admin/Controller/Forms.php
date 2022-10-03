6c033bb3fdc730112e9424c93c6e5414f193d049
Pablo de la Peña <p@shedcollective.org>
2022-10-03 12:25:06 +0100

    Refactor
    - Improve titling
    - Docblocks
    - Use/pass local variables
    - Fix CSV output

--- src/Admin/Controller/Forms.php
+++ src/Admin/Controller/Forms.php
@@ -24,11 +24,11 @@ use Nails\Common\Service\Input;
 use Nails\Common\Service\Uri;
 use Nails\CustomForms\Admin\Permission;
 use Nails\CustomForms\Constants;
-use Nails\CustomForms\Controller\BaseAdmin;
 use Nails\CustomForms\Model\Form;
 use Nails\CustomForms\Model\Response;
 use Nails\Factory;
 use Nails\FormBuilder;
+use Throwable;
 
 /**
  * Class Forms
@@ -40,7 +40,8 @@ class Forms extends Base
     /**
      * Announces this controller's navGroups
      *
-     * @return stdClass
+     * @return Nav|void
+     * @throws \Nails\Common\Exception\FactoryException
      */
     public static function announce()
     {
@@ -63,6 +64,8 @@ class Forms extends Base
      * Browse existing form
      *
      * @return void
+     * @throws \Nails\Common\Exception\FactoryException
+     * @throws \Nails\Common\Exception\ModelException
      */
     public function index()
     {
@@ -80,7 +83,7 @@ class Forms extends Base
         // --------------------------------------------------------------------------
 
         //  Set method info
-        $this->setTitles(['Browse Forms']);
+        $this->setTitles(['Custom Forms', 'Browse']);
 
         // --------------------------------------------------------------------------
 
@@ -136,6 +139,8 @@ class Forms extends Base
      * Create a new Form
      *
      * @return void
+     * @throws \Nails\Common\Exception\FactoryException
+     * @throws \Nails\Common\Exception\ModelException
      */
     public function create()
     {
@@ -168,9 +173,9 @@ class Forms extends Base
 
         // --------------------------------------------------------------------------
 
-        $this->loadViewData();
         $this
-            ->setTitles(['Create Form'])
+            ->loadViewData()
+            ->setTitles(['Custom Forms', 'Create'])
             ->loadView('edit');
     }
 
@@ -180,6 +185,8 @@ class Forms extends Base
      * Edit an existing Form
      *
      * @return void
+     * @throws \Nails\Common\Exception\FactoryException
+     * @throws \Nails\Common\Exception\ModelException
      */
     public function edit()
     {
@@ -194,8 +201,9 @@ class Forms extends Base
         /** @var Form $oFormModel */
         $oFormModel = Factory::model('Form', Constants::MODULE_SLUG);
 
-        $iFormId            = (int) $oUri->segment(5);
-        $this->data['form'] = $oFormModel->getById(
+        $iFormId = (int) $oUri->segment(5);
+        /** @var \Nails\CustomForms\Resource\Form $oForm */
+        $oForm = $oFormModel->getById(
             $iFormId,
             [
                 'expand' => [
@@ -215,16 +223,18 @@ class Forms extends Base
             ]
         );
 
-        if (empty($this->data['form'])) {
+        $this->data['form'] = $oForm;
+
+        if (empty($oForm)) {
             show404();
         }
 
         if ($oInput->post()) {
-            if ($this->runFormValidation([], $this->data['form'])) {
-                if ($oFormModel->update($iFormId, $this->getPostObject())) {
+            if ($this->runFormValidation([], $oForm)) {
+                if ($oFormModel->update($iFormId, $this->getPostObject($oForm))) {
 
                     $this->oUserFeedback->success('Form updated successfully.');
-                    redirect(static::url('edit/' . $this->data['form']->id));
+                    redirect(static::url('edit/' . $oForm->id));
 
                 } else {
                     $this->oUserFeedback->error('Failed to update form. ' . $oFormModel->lastError());
@@ -237,9 +247,9 @@ class Forms extends Base
 
         // --------------------------------------------------------------------------
 
-        $this->loadViewData();
         $this
-            ->setTitles(['Edit Form'])
+            ->loadViewData($oForm)
+            ->setTitles(['Custom Forms', 'Edit'])
             ->loadView('edit');
     }
 
@@ -250,7 +260,7 @@ class Forms extends Base
      *
      * @throws FactoryException
      */
-    protected function loadViewData()
+    protected function loadViewData(\Nails\CustomForms\Resource\Form $oForm = null): self
     {
         /** @var Captcha\Service\Captcha $oCaptcha */
         $oCaptcha = Factory::service('Captcha', Captcha\Constants::MODULE_SLUG);
@@ -268,14 +278,21 @@ class Forms extends Base
 
         if ($oInput->post()) {
             $this->data['aNotifications'] = $this->extractNotificationsFromPost();
-        } elseif (!empty($this->data['form'])) {
-            $this->data['aNotifications'] = $this->extractNotificationsFromObject();
+
+        } elseif (!empty($oForm)) {
+            $this->data['aNotifications'] = $this->extractNotificationsFromObject($oForm);
         }
+
+        return $this;
     }
 
     // --------------------------------------------------------------------------
 
-    private function extractNotificationsFromPost()
+    /**
+     * @return \stdClass[]
+     * @throws \Nails\Common\Exception\FactoryException
+     */
+    private function extractNotificationsFromPost(): array
     {
         /** @var Input $oInput */
         $oInput = Factory::service('Input');
@@ -294,7 +311,12 @@ class Forms extends Base
 
     // --------------------------------------------------------------------------
 
-    private function extractNotificationsFromObject()
+    /**
+     * @param \Nails\CustomForms\Resource\Form $oForm
+     *
+     * @return \stdClass[]
+     */
+    private function extractNotificationsFromObject(\Nails\CustomForms\Resource\Form $oForm): array
     {
         return array_map(function ($oItem) {
             $aItem = (array) $oItem;
@@ -306,7 +328,7 @@ class Forms extends Base
                 'condition_operator' => getFromArray('condition_operator', $aItem),
                 'condition_value'    => getFromArray('condition_value', $aItem),
             ];
-        }, $this->data['form']->notifications->data);
+        }, $oForm->notifications->data);
     }
 
     // --------------------------------------------------------------------------
@@ -314,11 +336,14 @@ class Forms extends Base
     /**
      * Form validation for edit/create
      *
-     * @param array $aOverrides Any overrides for the fields; best to do this in the model's describeFields() method
+     * @param array                                 $aOverrides Any overrides for the fields; best to do this in the model's describeFields() method
+     * @param \Nails\CustomForms\Resource\Form|null $oForm
      *
      * @return bool
+     * @throws \Nails\Common\Exception\FactoryException
+     * @throws \Nails\Common\Exception\ModelException
      */
-    protected function runFormValidation(array $aOverrides = [], $oForm = null)
+    protected function runFormValidation(array $aOverrides = [], \Nails\CustomForms\Resource\Form $oForm = null): bool
     {
         /** @var Form $oFormModel */
         $oFormModel = Factory::model('Form', Constants::MODULE_SLUG);
@@ -362,7 +387,7 @@ class Forms extends Base
                     'thankyou_page_body'     => [
                         function ($mBody) use ($oInput) {
                             $sTitle = trim($oInput->post('thankyou_page_title'));
-                            $mBody  = json_decode($oInput->post('thankyou_page_body'));
+                            $mBody  = json_decode($mBody);
                             if (empty($mBody) && empty($sTitle)) {
                                 throw new ValidationException(
                                     'Thank you page body is required if no title is set.'
@@ -399,14 +424,17 @@ class Forms extends Base
     /**
      * Extract data from post variable
      *
+     * @param \Nails\CustomForms\Resource\Form|null $oForm
+     *
      * @return array
+     * @throws \Nails\Common\Exception\FactoryException
      */
-    protected function getPostObject(): array
+    protected function getPostObject(\Nails\CustomForms\Resource\Form $oForm = null): array
     {
         Factory::helper('formbuilder', FormBuilder\Constants::MODULE_SLUG);
         /** @var Input $oInput */
         $oInput  = Factory::service('Input');
-        $iFormId = !empty($this->data['form']->form->id) ? $this->data['form']->form->id : null;
+        $iFormId = !empty($oForm->form->id) ? $oForm->form->id : null;
         $aData   = [
             'label'                  => trim($oInput->post('label')),
             'header'                 => trim($oInput->post('header')) ?: '[]',
@@ -435,7 +463,7 @@ class Forms extends Base
 
         /**
          * For fieldNumber:{\d} values we need to generate a signature so we can
-         * fetch the item later as the notificatuons require an Id, and this isn't
+         * fetch the item later as the notifications require an ID, and this isn't
          * available yet
          */
 
@@ -458,6 +486,8 @@ class Forms extends Base
      * Delete an existing form
      *
      * @return void
+     * @throws \Nails\Common\Exception\FactoryException
+     * @throws \Nails\Common\Exception\ModelException
      */
     public function delete()
     {
@@ -549,7 +579,7 @@ class Forms extends Base
      * @throws FactoryException
      * @throws ModelException
      */
-    protected function responsesList($oForm)
+    protected function responsesList($oForm): void
     {
         if (!userHasPermission(Permission\Response\Browse::class)) {
             unauthorised();
@@ -558,14 +588,17 @@ class Forms extends Base
         /** @var Response $oResponseModel */
         $oResponseModel = Factory::model('Response', Constants::MODULE_SLUG);
 
-        $this->setTitles(['Responses for form: ' . $oForm->label]);
+        $this->setTitles(['Custom Forms', $oForm->label, 'Responses']);
 
-        $this->data['form']      = $oForm;
-        $this->data['responses'] = $oResponseModel->getAll([
+        /** @var \Nails\CustomForms\Resource\Response[] $aResponses */
+        $aResponses = $oResponseModel->getAll([
             'where' => [
                 ['form_id', $oForm->id],
             ],
         ]);
+
+        $this->data['responses'] = $aResponses;
+        $this->data['form']      = $oForm;
 
         /** @var Input $oInput */
         $oInput = Factory::service('Input');
@@ -573,7 +606,6 @@ class Forms extends Base
 
             $aResults = [];
             $aColumns = [];
-            $oHeader  = reset($this->data['responses']);
 
             //  Extract all questions
             foreach ($this->data['responses'] as $oResponse) {
@@ -604,13 +636,23 @@ class Forms extends Base
                     }
                 }
 
-                $aResult[] = array_combine(
+                $aResults[] = array_combine(
                     array_merge(['Created'], $aColumns),
                     array_merge([(string) $oResponse->created], $aRow),
                 );
             }
 
-            Helper::loadCsv($aResult, $this->data['form']->slug . '.csv');
+            $aResults = array_merge(
+                [
+                    array_combine(
+                        array_merge(['Created'], $aColumns),
+                        array_merge(['Created'], $aColumns)
+                    ),
+                ],
+                $aResults
+            );
+
+            Helper::loadCsv($aResults, $oForm->slug . '.csv');
 
         } else {
             Helper::addHeaderButton(uri_string() . '?dl=1', 'Download as CSV');
@@ -628,7 +670,7 @@ class Forms extends Base
      *
      * @throws FactoryException
      */
-    protected function responseView($oResponse, $oForm)
+    protected function responseView($oResponse, $oForm): void
     {
         if (!userHasPermission(Permission\Response\View::class)) {
             unauthorised();
@@ -636,7 +678,7 @@ class Forms extends Base
 
         $this
             ->setData('response', $oResponse)
-            ->setTitles(['Responses for form: ' . $oForm->label])
+            ->setTitles(['Custom Forms', $oForm->label, 'Responses', 'Response #' . $oResponse->id])
             ->loadView('response');
     }
 
@@ -651,7 +693,7 @@ class Forms extends Base
      * @throws FactoryException
      * @throws ModelException
      */
-    protected function responseDelete($oResponse, $oForm)
+    protected function responseDelete($oResponse, $oForm): void
     {
         if (!userHasPermission(Permission\Response\Delete::class)) {
             unauthorised();
@@ -675,9 +717,8 @@ class Forms extends Base
      * Creates a copy of a form
      *
      * @throws FactoryException
-     * @throws ModelException
      */
-    public function copy()
+    public function copy(): void
     {
         if (!userHasPermission(Permission\Form\Create::class)) {
             unauthorised();
@@ -696,7 +737,7 @@ class Forms extends Base
             $this->oUserFeedback->success('Custom form was copied successfully.');
             $sRedirectUrl = static::url('edit/' . $iNewFormId);
 
-        } catch (\Exception $e) {
+        } catch (Throwable $e) {
             $this->oUserFeedback->error('Custom form failed to copy. ' . $e->getMessage());
             $sRedirectUrl = $oInput->get('return')
                 ? $oInput->get('return')
